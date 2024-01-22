9a9c983a248478cd309a2f325a9bc555a92eae68
Espen Kalvatn <espenk@stacc.com>
2024-01-22 21:21:42 +0100

    feat: build API for creating match

---
+++ app/api/matches/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
+import prisma from '@/prisma/client';
+
+const createMatchSchema = z.object({
+  homeTeam: z.string().min(1).max(255),
+  awayTeam: z.string().min(1).max(255),
+  homeScore: z.number().min(0).max(100),
+  awayScore: z.number().min(0).max(100),
+  date: z.string(),
+  stadium: z.string().min(1).max(255),
+  competition: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createMatchSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newIssue = await prisma.match.create({
+    data: {
+      homeTeam: body.homeTeam,
+      awayTeam: body.awayTeam,
+      homeScore: body.homeScore,
+      awayScore: body.awayScore,
+      date: body.date,
+      stadium: body.stadium,
+      competition: body.competition,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
