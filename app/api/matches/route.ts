144dfdf3dde4618b2b32a5ed492f32340657060b
Espen Kalvatn <espenk@stacc.com>
2024-01-25 08:56:43 +0100

    fix: added different response codes for matches endpoint

--- app/api/matches/route.ts
+++ app/api/matches/route.ts
@@ -4,28 +4,39 @@ import prisma from '@/prisma/client';
 import { createMatchSchema } from '@/app/validationSchemas';
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
-  const validation = createMatchSchema.safeParse(body);
-  if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
+  try {
+    const body = await request.json();
+    const validation = createMatchSchema.safeParse(body);
 
-  const newIssue = await prisma.match.create({
-    data: {
-      homeTeam: body.homeTeam,
-      awayTeam: body.awayTeam,
-      homeScore: body.homeScore,
-      awayScore: body.awayScore,
-      date: body.date,
-      stadium: body.stadium,
-      competition: body.competition,
-      description: body.description,
-    },
-  });
+    if (!validation.success)
+      return NextResponse.json(validation.error.errors, { status: 400 });
 
-  return NextResponse.json(newIssue, { status: 201 });
+    const newIssue = await prisma.match.create({
+      data: {
+        homeTeam: body.homeTeam,
+        awayTeam: body.awayTeam,
+        homeScore: body.homeScore,
+        awayScore: body.awayScore,
+        date: body.date,
+        stadium: body.stadium,
+        competition: body.competition,
+        description: body.description,
+      },
+    });
+
+    return NextResponse.json(newIssue, { status: 201 });
+  } catch (error) {
+    console.error('Error creating match:', error);
+    return NextResponse.json({ status: 500, error: 'Internal Server Error' });
+  }
 }
 
 export async function GET() {
-  const matches = await prisma.match.findMany();
-  return NextResponse.json(matches);
+  try {
+    const matches = await prisma.match.findMany();
+    return NextResponse.json(matches, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching matches:', error);
+    return NextResponse.json({ status: 500, error: 'Internal Server Error' });
+  }
 }
