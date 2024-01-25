655b160742e9b5c51ed0909055107ea6e06a4c0a
Espen Kalvatn <espenk@stacc.com>
2024-01-25 08:54:48 +0100

    feat: added matches/[id] endpoints

---
+++ app/api/matches/[id]/route.ts
@@ -0,0 +1,44 @@
+import prisma from '../../../../prisma/client';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  try {
+    const match = await prisma.match.findUnique({
+      where: {
+        id: Number(params.id),
+      },
+    });
+    if (!match) {
+      return NextResponse.json({ status: 404, error: 'Resource not found' });
+    }
+
+    return NextResponse.json(match, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching match:', error);
+    return NextResponse.json({ status: 500, error: 'Internal Server Error' });
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  try {
+    const match = await prisma.match.delete({
+      where: {
+        id: Number(params.id),
+      },
+    });
+
+    return NextResponse.json({
+      status: 200,
+      message: 'Resource deleted successfully',
+    });
+  } catch (error) {
+    console.error('Error deleting match:', error);
+    return NextResponse.json({ status: 500, error: 'Internal Server Error' });
+  }
+}
