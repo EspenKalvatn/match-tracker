4b77c246d7eef654d8674ead5b3e24acecc83ebc
Espen Kalvatn <espenk@stacc.com>
2024-01-27 20:31:38 +0100

    fix: added user id, name and email to client and server session

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,52 +1,5 @@
 import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import { compare } from 'bcrypt';
-import prisma from '@/prisma/client';
+import { authOptions } from '@/app/lib/auth';
 
-export const handler = NextAuth({
-  session: {
-    strategy: 'jwt',
-  },
-  pages: {
-    signIn: '/auth/signin',
-    newUser: '/auth/new-user',
-  },
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        email: {},
-        password: {},
-      },
-      async authorize(credentials, req) {
-        // TODO: add validation:
-
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials?.email,
-          },
-        });
-
-        console.log('user', user);
-
-        if (!user) return null;
-
-        const isValidPassword = await compare(
-          credentials?.password || '',
-          user.password,
-        );
-        console.log('isvalidpassword', isValidPassword);
-
-        if (isValidPassword) {
-          return {
-            id: user.id,
-            email: user.email,
-          };
-        }
-
-        return null;
-      },
-    }),
-  ],
-});
-
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
