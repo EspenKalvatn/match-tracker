f693a2fa40073fac600d0f6ea506264b557aff39
Espen Kalvatn <espenk@stacc.com>
2024-02-04 21:21:04 +0100

    fix: adding NEXTAUTH_SECRET

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -4,6 +4,7 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import { compare } from 'bcrypt';
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: 'jwt',
   },
