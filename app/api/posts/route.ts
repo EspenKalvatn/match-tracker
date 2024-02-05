5cdc2fe8c2a0e9a3c212c69defe02982bf89594d
Espen Kalvatn <espenk@stacc.com>
2024-02-05 18:07:58 +0100

    fix: ordering post by created at in descending order

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -36,6 +36,9 @@ export async function GET() {
           },
         },
       },
+      orderBy: {
+        createdAt: 'desc',
+      },
     });
     return NextResponse.json(posts, { status: 200 });
   } catch (error) {
